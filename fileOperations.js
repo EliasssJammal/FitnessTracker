7393c279cf57104f15d1cff75eb074152507e8bf
Elias <n01435671@humber.ca>
2023-09-07 11:07:53 -0400

    Initial commit

--- fileOperations.js
+++ fileOperations.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-//read a file
+// Read a file
 fs.readFile('example.txt', 'utf8', (err, data) => {
     if (err) {
         console.error('Error reading the file:', err);
